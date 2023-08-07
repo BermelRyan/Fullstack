10795527d834bb3cc431b13ed3d214a9979f072c
BermelRyan <111697855+BermelRyan@users.noreply.github.com>
2023-08-06 21:12:01 -0400

    Add files via upload

---
+++ app_server/controllers/travel.js
@@ -0,0 +1,12 @@
+var fs = require ('fs');
+var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
+
+/* Get travler view*/
+const travel = (req, res) => {
+    pageTitle = process.env.npm_package_description + ' - Travel';
+    res.render('travel', {title: pageTitle, trips});
+};
+
+module.exports = {
+    travel
+};
